34215eeb07dea716467365e2d5f121f8b7c8a7c0
Arnav Chauhan <arnav216@gmail.com>
2025-07-01 03:27:18 -0400

    Initial Commit

---
+++ quarterhour/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  quarterhour
+//
+//  Created by Arnav Chauhan on 7/1/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ quarterhour/quarterhourApp.swift
@@ -0,0 +1,17 @@
+//
+//  quarterhourApp.swift
+//  quarterhour
+//
+//  Created by Arnav Chauhan on 7/1/25.
+//
+
+import SwiftUI
+
+@main
+struct quarterhourApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
